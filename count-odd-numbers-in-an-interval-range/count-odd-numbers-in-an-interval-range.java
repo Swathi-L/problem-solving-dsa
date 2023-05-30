8ecaf6359b53c11fe156f89cb176c64ada0686ee
Swathi L <swathijubilant@gmail.com>
2023-05-30 10:44:52 -0700

    Time: 589 ms (5.36%) | Memory: 39.5 MB (61.75%) - LeetSync

---
+++ count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countOdds(int low, int high) {
+        int count=0;
+
+        if (low % 2 == 0) {
+            low = low + 1;
+        }
+        for(int i=low; i<=high; i=i+2) {
+            count++;
+        }
+        return count;
+    }
+}
