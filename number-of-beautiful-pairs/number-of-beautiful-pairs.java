f09c7ed9f69186e0e530cd62ace2e2beaf03ee69
Swathi L <swathijubilant@gmail.com>
2023-06-26 12:04:47 -0700

    Time: 35 ms (50.00%) | Memory: 43.1 MB (25.00%) - LeetSync

--- number-of-beautiful-pairs/number-of-beautiful-pairs.java
+++ number-of-beautiful-pairs/number-of-beautiful-pairs.java
@@ -11,15 +11,11 @@ class Solution {
     } 
     
     public int gcd(int n1, int n2) {
-        // System.out.println("n1: " + n1 + " n2: " + n2);
-        
         if(n2 > n1) {
             return gcd(n2, n1);
         }
         
-        int rem = n1 % n2; //rem = 3
-        // n1 = 17, n2 = 7
-        // n1 = 7
+        int rem = n1 % n2; 
         while(n2 > 1 && rem != 0) {
             n1 = n2;
             n2 = rem;
@@ -34,7 +30,6 @@ class Solution {
         for(int i=0; i<nums.length; i++) {
             for(int j=i+1; j<nums.length; j++) {
                 int gcd = gcd( firstDigit(nums[i]), nums[j] % 10);
-                // System.out.println("gcd: "+ gcd);
                 if(gcd == 1) {
                     count++;
                 }
