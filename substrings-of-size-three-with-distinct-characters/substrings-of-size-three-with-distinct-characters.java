6bf28d14e64d0d109effa044be3e228e257dfe50
Swathi L <swathijubilant@gmail.com>
2023-07-07 16:51:09 -0700

    Time: 3 ms (40.89%) | Memory: 41 MB (45.91%) - LeetSync

--- substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
+++ substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
@@ -1,45 +1,45 @@
 class Solution {
     public int countGoodSubstrings(String s) {
-        Map<Character, Integer> charSet = new HashMap<>();
+        Map<Character, Integer> charCount = new HashMap<>();
         int max_count = 0;
 
         if(s.length() < 3){ return max_count; }
 
         for(int i=0; i<3; i++) {
             char c = s.charAt(i);
-            if(!charSet.containsKey(c)) {
-                charSet.put(c, 1);
+            if(!charCount.containsKey(c)) {
+                charCount.put(c, 1);
             }else {
-                int count = charSet.get(c);
-                charSet.put(c, count+1);
+                int count = charCount.get(c);
+                charCount.put(c, count+1);
             }
             
         }
 
-        if(charSet.size() == 3) {max_count++; }
+        if(charCount.size() == 3) {max_count++; }
 
         int p1 = 0, p2 = 3;
 
         while(p2 < s.length()) {
             char p1_char = s.charAt(p1);
-            int count = charSet.get(p1_char);
+            int p1_count = charCount.get(p1_char);
             
-            if(count - 1 > 0) {
-                 charSet.put(p1_char, count-1);
+            if(p1_count - 1 > 0) {
+                 charCount.put(p1_char, p1_count-1);
             } else {
-                charSet.remove(p1_char);
+                charCount.remove(p1_char);
             }
            
             char p2_char = s.charAt(p2);
             
-            if(!charSet.containsKey(p2_char)) {
-                charSet.put(p2_char, 1);
+            if(!charCount.containsKey(p2_char)) {
+                charCount.put(p2_char, 1);
             } else {
-                int p2_count = charSet.get(p2_char);
-                charSet.put(p2_char, p2_count+1);
+                int p2_count = charCount.get(p2_char);
+                charCount.put(p2_char, p2_count+1);
             }
 
-            if(charSet.size() == 3) { max_count++; }
+            if(charCount.size() == 3) { max_count++; }
             p1++;
             p2++;
         }
