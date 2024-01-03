bd5e3b6364465fe517c0baad632a534af2c6781c
Swathi L <swathijubilant@gmail.com>
2024-01-02 22:25:51 -0800

    Time: 131 ms (42.87%) | Memory: 127.4 MB (74.72%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,6 +1,5 @@
 class Solution {
     public void buildAdj(int n, int[][] edges, List<List<Integer>> adj) {
-
         for(int i=0; i<n; i++) {
             adj.add(new ArrayList<>());
         }
@@ -11,18 +10,22 @@ class Solution {
         }
     }
 
-    public boolean dfs(int src, int dest, List<List<Integer>> adj, boolean[] visited) {
-        if(src == dest) {
-            return true;
-        }
-        if(visited[src]) {
-            return false;
-        }
+    public boolean bfs(int src, int dest, List<List<Integer>> adj, boolean[] visited) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(src);
         visited[src] = true;
 
-        for(int s : adj.get(src)) {
-            if(dfs(s, dest, adj, visited)) {
+        while(q.size() > 0) {
+            int node = q.remove();
+            if(node == dest) {
                 return true;
+            }
+
+            for(int n : adj.get(node)) {
+                if(!visited[n]) {
+                    visited[n] = true;
+                    q.add(n);
+                }
             }
         }
         return false;
@@ -31,8 +34,8 @@ class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         List<List<Integer>> adj = new ArrayList<>();
         buildAdj(n, edges, adj);
-
-        boolean[] visited = new boolean[n];
-        return dfs(source, destination, adj, visited);
+        
+        boolean visited[] = new boolean[n];
+        return bfs(source, destination, adj, visited);
     }
 }
