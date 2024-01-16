e28a6ab736525ba284176b3041bfd9431449571a
Swathi L <swathijubilant@gmail.com>
2024-01-15 19:23:38 -0800

    Time: 0 ms (100.00%) | Memory: 56.1 MB (54.17%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -4,14 +4,12 @@ class Solution {
 
         while(l < r) {
             int mid = l + (r-l)/2;
-            if(arr[mid] > arr[mid+1] && arr[mid] > arr[mid-1]) {
-                return mid;
-            }
-            if(arr[mid-1] >= arr[mid+1]) {
-                r = mid - 1;
+
+            if(arr[mid+1] >= arr[mid]) {
+                l = mid + 1;
             }
             else {
-                l = mid+1;
+                r = mid;
             }
         }
         return l;
