237baa0717b06a8a050030baa7a71505302b77f3
Swathi Loganathan <swathijubilant@gmail.com>
2025-08-23 11:27:27 -0700

    Time: 4 ms (49.21%) | Memory: 49.5 MB (72.93%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,31 +1,35 @@
 class Solution {
-    public void dfs(int row, int col, char[][] grid){
-        if(row<0 || row >= grid.length || col >= grid[0].length || col < 0) {
-            return;
-        }
-        if(grid[row][col] == '0') {return; }
-        grid[row][col] = '0';
-        dfs(row, col+1, grid); //right
-        dfs(row, col-1, grid); //left
-        dfs(row-1, col, grid); //up
-        dfs(row+1, col, grid); //down
-    }
-
+    int[][] dirs = {{0,1}, {0,-1}, {-1,0}, {1,0}};
+    
     public int numIslands(char[][] grid) {
-       /*
-Vertices: In the context of the "Number of Islands" problem, the vertices can be thought of as the individual cells of the 2D grid. Each cell can be considered a node or a vertex.
-
-Edges: The edges represent the connections between the cells in the 2D grid. In this problem, two adjacent cells (up, down, left, or right, but not diagonally) with a value of '1' are considered to be connected. Therefore, the edges represent the connections between adjacent land cells.
-        */ 
-        int islands = 0;
+        
+        int count = 0;
         for(int i=0; i<grid.length; i++) {
-            for(int j=0; j<grid[i].length; j++) {
+            for(int j=0; j<grid[0].length; j++){
                 if(grid[i][j] == '1') {
-                    dfs(i, j, grid);
-                    islands++;
+                    dfs(grid, i, j);
+                    count++;
                 }
             }
         }
-        return islands;
+        return count;
+    }
+
+    void dfs(char[][] grid, int r, int c) {
+        if(r < 0 || c < 0 || r >= grid.length || c >= grid[0].length
+            || grid[r][c] == '0') {
+             return;
+        }
+
+        // mark as visited
+        grid[r][c] = '0';
+
+        // explore all directions
+        for(int[] dir : dirs) {
+            int row = dir[0] + r;
+            int col = dir[1] + c;
+
+            dfs(grid, row, col);
+        }
     }
 }
