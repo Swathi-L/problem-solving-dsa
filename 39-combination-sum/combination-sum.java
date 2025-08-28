798eb5d1094400a0f979e758510489b07cf520ea
Swathi Loganathan <swathijubilant@gmail.com>
2025-08-27 18:16:21 -0700

    Time: 2 ms (83.59%) | Memory: 44.8 MB (45.71%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,11 +1,14 @@
 class Solution {
-    List<List<Integer>> res = new ArrayList<>();
+    
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-       helper(candidates, target, 0, new ArrayList<>()); 
+       List<List<Integer>> res = new ArrayList<>();
+
+       helper(candidates, target, 0, new ArrayList<>(), res); 
        return res;
     }
 
-    void helper(int[] arr, int target, int i, List<Integer> combo) {
+    void helper(int[] arr, int target, int i, List<Integer> combo, 
+    List<List<Integer>> res) {
 
         if(target == 0) {
             res.add(new ArrayList<>(combo));
@@ -17,11 +20,11 @@ class Solution {
         }
 
         // not choose
-        helper(arr, target, i+1, combo);
+        helper(arr, target, i+1, combo, res);
 
         //choose
         combo.add(arr[i]);
-        helper(arr, target-arr[i], i, combo);
+        helper(arr, target-arr[i], i, combo, res);
         
         // backtrack
         combo.remove(combo.size()-1);    
