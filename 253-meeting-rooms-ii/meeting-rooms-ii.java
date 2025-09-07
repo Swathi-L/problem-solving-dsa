b90ffd0be16587f5cd4b4a3564c7a0f9afe25a48
Swathi Loganathan <swathijubilant@gmail.com>
2025-09-07 09:59:10 -0700

    Time: 8 ms (68.58%) | Memory: 46 MB (68.62%) - LeetSync

---
+++ 253-meeting-rooms-ii/meeting-rooms-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        PriorityQueue<Integer> endTimes = new PriorityQueue<>();
+
+        for(int[] interval : intervals) {
+            
+            if(endTimes.isEmpty()) {
+                endTimes.add(interval[1]);
+            }
+            else {
+                int endtime = endTimes.peek();
+                if(endtime > interval[0]) {
+                    endTimes.add(interval[1]);
+                }
+                else {
+                    endTimes.poll();
+                    endTimes.add(interval[1]);
+                }
+            }
+        }
+        return endTimes.size();
+    }
+}
